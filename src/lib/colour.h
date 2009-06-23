930037ab0d347dc2743dce5698fa4fc6a835da50
amitavmohanty01 <amitavmohanty01@17e3cb8c-16c5-11de-9433-ad2e3fad96c5>
2009-06-23 21:50:14 +0000

    eina array used for the table.

--- src/lib/colour.h
+++ src/lib/colour.h
@@ -1,8 +1,10 @@
 #ifndef COLOUR_H
 #define COLOUR_H
 #include <stdio.h>
+#include <eina_array.h>
 typedef struct colour{
   char r,g,b;
 }COLOUR;
 int ertf_colortbl(FILE *);
+Eina_Array *colour_table;
 #endif
