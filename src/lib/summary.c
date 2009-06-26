035eb73f7e62fc195f0e1e6f641f6c86af8c027c
amitavmohanty01 <amitavmohanty01@17e3cb8c-16c5-11de-9433-ad2e3fad96c5>
2009-06-26 18:53:49 +0000

    adding summary group handlers.

---
+++ src/lib/summary.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "summary.h"
+#include <string.h>
+static int ertf_resolve_control_word(FILE *);
+// todo: deal with the case of multiple \info tags
+/*
+ * This function is called when \info tag is encountered.
+ */
+int ertf_summary(FILE *fp){
+
+  int c;
+  doc_info=(INFO *)malloc(sizeof(INFO));
+
+  // default values
+  doc_info->author=NULL;
+  doc_info->title=NULL;
+  doc_info->subject=NULL;
+  doc_info->operator=NULL;
+  doc_info->keywords=NULL;
+  doc_info->comment=NULL;
+  doc_info->version=0;// zero indicates not set
+  doc_info->doccomm=NULL;
+  doc_info->internal_version=0;
+  doc_info->pages=-1;
+  doc_info->words=-1;
+  doc_info->chars=-1;
+  doc_info->internal_ID=0;
+  // todo: update for default of time variables
+
+  // obtain keywords till the end of group with proper error checking
+  while((c=fgetc(fp))!=EOF){
+    switch(c){
+    case '{':
+      if(ertf_resolve_control_word(fp))
+	break;
+      else
+	goto error;
+    case '}':return 1;
+    default:
+      fprintf(stderr, "ertf_summary: Ill-formatted rtf file.\n");
+      goto error;
+    }
+  }
+
+  // free it in case of EOF and in presence of invalid characters
+ error:
+  free(doc_info);
+  return 0;
+}
+/*
+ * This function reads each summary group till the end brace (including it).
+ */
+static int ertf_resolve_control_word(FILE *fp){
+  int c;
+  char control_word[12];
+  // keeping it a multiple of four
+
+  if((c=fgetc(fp)) == EOF || c != '\\'){
+    fprintf(stderr, "Ill-formed rtf.\n");
+    return 0;
+  }
+  fscanf(fp, "%[^ ]", control_word);
+  // get control word
+  if(feof(fp)){
+    fprintf(stderr, "End of file reached.\n");
+    return 0;
+  }
+  // resolve the control word
+  switch(control_word[0]){
+  case 'a':// \author
+    if(strcmp(control_word+1, "uthor")==0){
+      // the keyword is actually \author
+      // todo: process it
+      return 1;
+    }else
+      goto skip;
+  case 'b':// \buptime
+  case 'c':// \creatim, \comment
+  case 'd':// \doccomm, \dy
+  case 'e':// \edmins
+  case 'h':// \hr
+  case 'i':// \id
+  case 'k':// \keywords
+  case 'm':// \mo, \\min
+  case 'n':// \nofpages, \nofwords, \nofchars
+  case 'o':// \operator
+  case 'p':// \printim
+  case 'r':// \revtim
+  case 's':// \subject
+  case 't':// \title
+  case 'v':// \version, \vern
+  case 'y':// \yr
+    if(strcmp(control_word+1, "r")==0){
+      // todo: process
+      return 1;
+    }else{
+      goto skip;
+    }
+    //default: skip invaid tag
+  }
+ skip:
+  fprintf(stderr, "summary: skipping control tag %s\n", control_word);
+  return 1;
+}
