538467bffb1cf577c23171a885542a8c135a9549
amitavmohanty01 <amitavmohanty01@17e3cb8c-16c5-11de-9433-ad2e3fad96c5>
2009-08-15 04:44:39 +0000

    - colour parsing - improving font size handling

--- trunk/src/lib/rtf_to_markup.c
+++ trunk/src/lib/rtf_to_markup.c
@@ -3,36 +3,29 @@
 #include <ctype.h>
 #include "input.h"
 
-int ertf_paragraph_translate(FILE *);
-static int ertf_group_translate(FILE *, int);
+// \plain is ignored and the text part is copied as markup and evas
+// textblock uses the style set by default
 
-  // \plain is ignored and the text part is copied as markup and evas
-  // textblock uses the style set by default
+// todo: \deleted not implemented in the initial version
 
-  // todo: \deleted not implemented in the initial version
+// \dn<N> translates to ?
 
-  // \dn<N> translates to ?
+// \f translates to ?
 
-  // \f translates to ? Can the font be set in the markup or does it have to be
-  // set in textblock style? In the latter case, when a new group is encountered
-  // in the rtf document a new style can be created in the style eina_array and
-  // its name can be used in generating the markup. In the former case, it would
-  // easier if something like <font=Verdana ...> is allowed within markup
+// \strike translates to <strikethough=on ...>
 
-  // \strike translates to <strikethough=on ...>
+// \scaps translates to ?
 
-  // \scaps translates to ?
+// \uld translates to ?
 
-  // \uld translates to ?
-
-
-int ertf_paragraph_translate(FILE *fp){
+int ertf_paragraph_translate(FILE *fp, int align){
   int c;
   char buf[12];
-
+  int fontset=0;
+  /*
   strcpy(markup+ertf_markup_position, "<p>");
   ertf_markup_position+=3;
-
+  */
   while((c=fgetc(fp))!=EOF){
     switch(c){
     case '\n':// ignore it
@@ -40,7 +33,7 @@ int ertf_paragraph_translate(FILE *fp){
 
       /* get control word */
     case '\\':
-      fscanf(fp, "%[^ 0123456789\\{]", buf);
+      fscanf(fp, "%[^ 0123456789\\{}]", buf);
       CHECK_EOF(fp, "ertf_paragraph_translate: end-of-file encountered while "
 		"retrieving control word.\n", return 0);
 
@@ -52,6 +45,21 @@ int ertf_paragraph_translate(FILE *fp){
       else if(strcmp(buf, "s")==0){
 	// todo: ensure that all stylesheet entries have been parsed
 	// todo: add the relevant style string to markup
+	fgetc(fp);
+	CHECK_EOF(fp, "ertf_paragraph_translate: end-of-file encountered while "
+		"retrieving style number.\n", return 0);
+      }
+
+      /* right aligned text */
+      else if(strcmp(buf, "rtlch")==0){
+	// todo: either insert align=right in markup or in a style
+	strcpy(markup+ertf_markup_position, "<right>");
+	ertf_markup_position+=7;
+
+	if(!ertf_paragraph_translate(fp, 1))
+	  return 0;
+	strcpy(markup+ertf_markup_position, "</right>");
+	ertf_markup_position+=8;
       }
 
       /* end of paragraph */
@@ -59,66 +67,12 @@ int ertf_paragraph_translate(FILE *fp){
 	// todo: ensure that </p> is defined in style string
 	strcpy(markup+ertf_markup_position, "</p>");
 	ertf_markup_position+=4;
-	return 1;
-      }
-
-      /* unsupported/unrecognised control tag */
-      else{
-	fprintf(stderr, "ertf_paragraph_translate: skipped control tag `%s'\n", buf);
-
-	/* read till next delimiter */
-	while((c=fgetc(fp))!='\\' && c != ' ' && c!='{')
-	  CHECK_EOF(fp, "ertf_paragraph_translate: EOF reached while skipping"
-		    " control info.\n", return 0);
-	ungetc(c, fp);
-      }
-      break;
-
-      /* handle group */
-    case '{':
-      if(ertf_group_translate(fp, 0)){
-	return 1;
-      }else
-	return 0;
-      break;
-
-    default:
-      fprintf(stderr, "ertf_paragraph_translate: unrecognised control"
-	      " character `%c'\n", c);
-    }
-  }
-  fprintf(stderr, "ertf_paragraph_translate: EOF encountered while looping for"
-	  " control word.\n");
-  return 0;
-}
-
-// align = 0 for left and 1 for right
-static int ertf_group_translate(FILE *fp, int align){
-  int c;
-  char buf[12];
-
-  while((c=fgetc(fp))!=EOF){
-    switch(c){
-      /* get control word */
-    case '\\':
-      fscanf(fp, "%[^ 0123456789\\{}]", buf);
-      CHECK_EOF(fp, "ertf_group_translate: EOF reached while reading control word.\n", return 0);
-
-      /* right aligned text */
-      if(strcmp(buf, "rtlch")==0){
-	// todo: either insert align=right in markup or in a style
-	strcpy(markup+ertf_markup_position, "<right>");
-	ertf_markup_position+=7;
-
-	if(!ertf_group_translate(fp, 1))
-	  return 0;
-	strcpy(markup+ertf_markup_position, "</right>");
-	ertf_markup_position+=8;
+	goto success;
       }
 
       /* left aligned text */
-      else if(strcmp(buf, "ltrch")==0 && align){
-	return 1;	
+      else if(strcmp(buf, "ltrch")==0 && align==1){
+	goto success;
       }
 
       /* italicized text */
@@ -150,15 +104,17 @@ static int ertf_group_translate(FILE *fp, int align){
 	// todo: check if underline colour needs to be specified
 	strcpy(markup+ertf_markup_position, "<underline=on>");
 	ertf_markup_position+=14;
-	if(!ertf_group_translate(fp, align))
+	if(!ertf_paragraph_translate(fp, align))
 	  return 0;
 	strcpy(markup+ertf_markup_position, "</>");
 	ertf_markup_position+=3;
       }
-      
+
       /* font size */
-      else if(strcmp(buf, "fs")==0){
-	/* use the digit character instead of reading an int and converting it back to character for markup */
+      else if(strcmp(buf, "fs")==0 && fontset==0){
+	/* use the digit character instead of reading an int and converting it 
+	 * back to character for markup
+	 */
 
 	strcpy(markup+ertf_markup_position, "<font_size=");
 	ertf_markup_position+=11;
@@ -170,63 +126,57 @@ static int ertf_group_translate(FILE *fp, int align){
 	ungetc(c, fp);
 	markup[ertf_markup_position]='>';
 	ertf_markup_position++;
-	if(!ertf_group_translate(fp, align))
-	  return 0;
-	strcpy(markup+ertf_markup_position, "</>");
-	ertf_markup_position+=3;
+	fontset=1;
       }
 
-      /* end of paragraph */
-      else if(strcmp(buf, "par")==0){
-	// todo: ensure that </p> is defined in style string
-	strcpy(markup+ertf_markup_position, "</p>");
-	ertf_markup_position+=4;
-	return 1;
-      }
-
-      /* unrecognised/unsupported control word */
-      else{
-	fprintf(stderr, "ertf_group_translate: Skipped control tag `%s'\n", buf);
-	/* read till next delimiter */
-	while((c=fgetc(fp))!='\\' && c != ' ' && c!='{')
-	  CHECK_EOF(fp, "ertf_group_translate: EOF reached while skipping control info.\n", return 0);
+      /* handle target */
+      else if(strcmp(buf, "*")==0){
+	/* They not supported in the prototype. */
+	while((c=fgetc(fp))!=EOF && c!='}')
+	  ;
+	CHECK_EOF(fp, "ertf_group_translate: EOF reached while handling unsupported target.\n", return 0);
 	ungetc(c, fp);
       }
 
+      /* unsupported/unrecognised control tag */
+      else{
+	fprintf(stderr, "ertf_paragraph_translate: skipped control tag `%s'\n", buf);
+
+	/* read till next delimiter */
+	while((c=fgetc(fp))!='\\' && c != ' ' && c!='{' && c!= '}')
+	  CHECK_EOF(fp, "ertf_paragraph_translate: EOF reached while skipping"
+		    " control info.\n", return 0);
+	ungetc(c, fp);	
+      }
       break;
 
-      /* handle nested group */
+      /* handle group */
     case '{':
-      if(!ertf_group_translate(fp, align))
+      if(!ertf_paragraph_translate(fp, align))
 	return 0;
       break;
 
       /* end of group */
     case '}':
-      return 1;
-
-      /* handle target */
-    case '*':
-      /* They not supported in the prototype. */
-      while((c=fgetc(fp))!=EOF && c!='}')
-	;
-      CHECK_EOF(fp, "ertf_group_translate: EOF reached while handling unsupported target.\n", return 0);
-      /* The end brace of the group is handled here and the loop continues with the next control word. */
-      break;
+      goto success;
 
     default:
       if(c=='\n'){
 	strcpy(markup+ertf_markup_position, "<br>");
 	ertf_markup_position+=4;
       }else{
-      markup[ertf_markup_position]=c;
-      ertf_markup_position++;
-	}
+	markup[ertf_markup_position]=c;
+	ertf_markup_position++;
+      }
     }
-  }// end of while
-
-  fprintf(stderr, "ertf_group_translate: End-of-file reached.%d\n", c);
-  // todo: confirm if this should return one and complete the string
-  // todo: may be keep partial rendering optional
+  }
+  fprintf(stderr, "ertf_paragraph_translate: EOF encountered while looping for"
+	  " control word.\n");
   return 0;
+ success:
+  if(fontset){
+    strcpy(markup+ertf_markup_position, "</>");
+    ertf_markup_position+=3;
+  }
+  return 1;
 }
