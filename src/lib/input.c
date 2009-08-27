f31649584bad2a0b3833c346b5dc37e7cef6858c
amitavmohanty01 <amitavmohanty01@17e3cb8c-16c5-11de-9433-ad2e3fad96c5>
2009-08-27 17:10:58 +0000

    replaced strcpy() with memcpy()

--- src/lib/input.c
+++ src/lib/input.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "input.h"
 
 /* keep track of the buffer's current size */
 int current_size = 0;
-
-/* fast implementation if strncpy, without error checking */
-static void _strncpy(char *, char *, int n);
 
 int
 ertf_markup_add(char *string, int len)
@@ -16,12 +14,12 @@ ertf_markup_add(char *string, int len)
 
   if (ertf_markup_position + len < current_size)
   {
-    _strncpy(markup + ertf_markup_position, string, len);
+    memcpy(markup + ertf_markup_position, string, len);
   }
   else
   {
     temp = ertf_markup_position + len - current_size;
-    _strncpy(markup + ertf_markup_position,
+    memcpy(markup + ertf_markup_position,
 	     string,
 	     len - temp
 	     );
@@ -31,24 +29,15 @@ ertf_markup_add(char *string, int len)
       fprintf(stderr, "ertf_markup_add: Out of memory while allocating markup.\n");
       return 0;
     }
-    _strncpy(tmp, markup, current_size);
+    memcpy(tmp, markup, current_size);
     if (markup)
     {
       free(markup);
     }
     markup = tmp;
-    _strncpy(markup + current_size, string + len - temp, temp);
+    memcpy(markup + current_size, string + len - temp, temp);
     current_size += 1024;
   }
   ertf_markup_position += len;
   return 1;
-}
-
-static void
-_strncpy(char *dest, char *src, int n)
-{
-  int i;
-
-  for(i = 0; i < n; i++)
-    dest[i] = src[i];
 }
