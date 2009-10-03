10c42c2b025470bcc1c081a4b4c442d439a9fb6e
amitavmohanty01 <amitavmohanty01@17e3cb8c-16c5-11de-9433-ad2e3fad96c5>
2009-10-03 19:41:30 +0000

    * started margin support * added default margin values

--- src/lib/ertf_private.h
+++ src/lib/ertf_private.h
@@ -9,6 +9,13 @@ extern unsigned char _ertf_default_color_r;
 extern unsigned char _ertf_default_color_g;
 extern unsigned char _ertf_default_color_b;
 
+extern int _ertf_default_paper_width;
+extern int _ertf_default_paper_height;
+extern int _ertf_default_left_margin;
+extern int _ertf_default_right_margin;
+extern int _ertf_default_top_margin;
+extern int _ertf_default_bottom_margin;
+
 extern Eina_Array *color_table;
 extern Eina_Array *font_table;
 extern Eina_Array *stylesheet_table;
