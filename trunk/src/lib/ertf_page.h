5e6b86c1e1be8a1883fecc7e232859667d4204d1
amitavmohanty01 <amitavmohanty01@17e3cb8c-16c5-11de-9433-ad2e3fad96c5>
2010-03-08 21:45:56 +0000

    * adding the page header and source which I forgot to add in previous commit

---
+++ trunk/src/lib/ertf_page.h
@@ -0,0 +1,19 @@
+#ifndef __ERTF_PAGE_H__
+#define __ERTF_PAGE_H__
+
+#include <Evas.h>
+
+typedef struct Ertf_Page Ertf_Page;
+
+Ertf_Page *ertf_page_new (const Ertf_Document *doc);
+
+void ertf_page_free (Ertf_Page *page);
+
+void ertf_page_render (Ertf_Page *page, Evas *evas);
+
+void ertf_page_page_set (Ertf_Page *page, int p);
+
+int ertf_page_page_get (Ertf_Page *page);
+
+
+#endif /* __ERTF_PAGE_H__ */
