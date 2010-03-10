f06638f7343e88b54199bd49c3fcc7e98ea74cec
amitavmohanty01 <amitavmohanty01@17e3cb8c-16c5-11de-9433-ad2e3fad96c5>
2010-03-10 22:42:15 +0000

    * negative page numbers are equivalent to zeroth page

--- trunk/src/lib/ertf_page.c
+++ trunk/src/lib/ertf_page.c
@@ -59,6 +59,11 @@ ertf_page_render (Ertf_Page *page, Evas_Object *textblock)
     _ertf_document_generate_pages(textblock, page->doc);
   }
   evas_object_textblock_clear(textblock);
+  if (page->page < 0)
+    page->page = 0;
+  if (page->page >= eina_array_count_get(page->doc->pages))
+    page->page = eina_array_count_get(page->doc->pages) - 1;
+  printf("count:%d\n", page->page);
   markup_text = eina_array_data_get(page->doc->pages, page->page);
   evas_object_textblock_text_markup_set(textblock, markup_text);
 }
